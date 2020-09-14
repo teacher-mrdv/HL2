ed7888a63e1845592e648258b48d0e01f6572b10
teacher Marcos <teacher.mrdv@gmail.com>
2020-09-14 11:56:06 +0800

    updates Monday

--- BST/src/BSTTest.java
+++ BST/src/BSTTest.java
@@ -5,21 +5,7 @@ public class BSTTest
 	public static void main(String[] args)
 	{
 		// your code goes here
-		BSTree myBST = new BSTree(5);
-		System.out.println();
-		myBST.add(myBST.root, 3);
-		myBST.add(myBST.root, 9);
-		myBST.add(myBST.root, 1);
-		myBST.add(myBST.root, 4);
-		myBST.add(myBST.root, 6);
-		myBST.add(myBST.root, 10);
-		myBST.add(myBST.root, 8);
-		System.out.println();
-		System.out.println("Is 9 in the tree? " +
-				myBST.isFound(myBST.root, 9));
-		System.out.println("Is 0 in the tree? " +
-				myBST.isFound(myBST.root, 0));
-		myBST.inOrder(myBST.root);
+
 	}
 
 }
